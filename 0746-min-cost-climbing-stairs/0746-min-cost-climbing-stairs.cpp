202f6ddb7b7a39483c293973c3fd520802071b24
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-06-02 11:43:15 +0530

    Time: 11 ms (5.97%), Space: 16.5 MB (17.47%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,35 +1,24 @@
 #include <vector>
-#include <algorithm> // for min
+#include <algorithm>
 using namespace std;
 
 class Solution {
-private:
-    int solve(vector<int>& cost, int n, int i,vector<int>&dp) {
-        // Base case: if the current index reaches or exceeds the top of the stairs
-        if (i >= n) {
-            return 0;
-        }
-        if(dp[i]!=-1){
-          return dp[i];
-        }
-        
-        // Calculate the minimum cost for reaching the top from the current index
-        int minCost = cost[i] + min(solve(cost, n, i + 1,dp), solve(cost, n, i + 2,dp));
-         
-        dp[i]= minCost;
-        return dp[i];
-    }
-
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        //memo
-        vector<int>dp(n+1,-1);
         
-        // Calculate the minimum cost starting from the bottom step
-        int minCostFromBottom = min(solve(cost, n, 0,dp), solve(cost, n, 1,dp));
+        // Initialize the DP array with the costs of the first two steps
+        vector<int> dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         
-        // Return the minimum cost from either starting from the 0th or 1st index
-        return minCostFromBottom;
+        // Iterate from the third step to the end
+        for (int i = 2; i < n; ++i) {
+            // Update dp[i] based on the recurrence relation
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        
+        // Return the minimum of the costs of the last two steps
+        return min(dp[n-1], dp[n-2]);
     }
 };
