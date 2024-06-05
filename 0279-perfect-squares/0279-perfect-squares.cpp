2b388992668e6eaa1dfc715b5fc39c7aa7117579
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-06-05 10:05:27 +0530

    Time: 82 ms (42.32%), Space: 11.5 MB (23.49%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,46 +1,26 @@
-/*class Solution {
+class Solution {
     
     private:
-    int solve(int n,int num,int cnt )
+    int solve(int num,vector<int>&dp)
     {
         int mini =INT_MAX;
-      if(n==num){
-          return cnt;
+      if(num==0){
+          return 0;
       }
-        for(int i=1;i<n;i++){
-     int minicnt=   solve(n,num-i*i,cnt+1);
-            if(minicnt<mini){
-          mini=minicnt;
-            }
+        if(dp[num]!=-1){
+            return dp[num];
+        }
+        for(int i=1;i*i<=num;i++){
+     int minicnt= 1+  solve(num-i*i,dp);
+          mini=min(minicnt,mini);
     }
+        dp[num]=mini;
+        return dp[num];
     }
 public:
     int numSquares(int n) {
-        
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
-*/
-class Solution {
-public:
-int solve(int n)
-{
-        vector<int> dp(n+1,INT_MAX);
-        dp[0] = 0;
-    for(int j = 1;j<=n;j++)
-    {
-        int mini = INT_MAX;
-        for(int i = 1;i<=sqrt(j);i++)
-       {
-           int sq = i*i;
-           int curr = 1 + dp[j - sq];
-           mini = min(mini,curr);
-       }
-       dp[j] = mini;
-    }
-    return dp[n];
-}
-    int numSquares(int n) 
-    {
-        return solve(n);
-    }
-};
+
