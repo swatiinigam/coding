504b7378d79b31e81abcd038e6805d0d3d63cd10
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-08-25 12:28:09 +0530

    Time: 6 ms (17.96%), Space: 14.7 MB (62.31%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+         next_permutation(nums.begin(),nums.end());
+    }
+};
